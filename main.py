606080d54e2a9e3922e5530f2d2c290fb6c9f59e
masoodjanbackloo <47041417+masoodjanbackloo@users.noreply.github.com>
2022-08-22 15:24:53 +0430

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from arithmetic_arranger import arithmetic_arranger
+arithmetic_arranger(["134 + 698", "3801 - 2", "5553 + 43", "123 - 49" ,"1233 + 49" ] , True )
+print("#############################################")
+arithmetic_arranger(["32 + 698", "3801 - 2", "5553 + 43", "123 + 49" ,"123 + 49"  ]  )
